8284a272c3139a452621a31e5c06d1dc31d5d29b
MiguelRamosLop <miguelon2001b@gmail.com>
2022-06-06 14:17:22 +0200

    upload cache updated_at

--- app/Http/Controllers/noticiaController.php
+++ app/Http/Controllers/noticiaController.php
@@ -79,36 +79,29 @@ class noticiaController extends Controller
                 $inmueble->caracteristicas = $item->caracteristicas;
                 $inmueble->save();
             }
-        } 
-        $inmu = Inmuebles::where('localizaciones_id', '=', $id_localidad)->where('tipo', '=', $tipo)->get();
-        for ($i=0; $i < count($inmu); $i++) { 
-            if (strtotime($inmu[$i]->updated_at) < strtotime('-30 days')) {
-                $nombre = $inmu[$i]->nombre;
-                $inmu[$i]->delete();
-                $c = '"..\resources\py\scraper_yaencontre.py" '.$localidad." ".$tipo;
-                $result = exec('python '.$c);
-                $json_clean = str_replace("'","\"",$result);
-                $json = json_decode($json_clean);
-                foreach ($json as $item) {
-                    if ($item->nombre == $nombre) {
-                        $inmu[$i] = new Inmuebles;
-                        $inmu[$i]->nombre = $item->nombre;
-                        $inmu[$i]->precio = $item->precio;
-                        $inmu[$i]->localizaciones_id = $id_localidad;
-                        $inmu[$i]->imagenes = $item->imagenes;
-                        $inmu[$i]->descripcion = $item->descripcion;
-                        $inmu[$i]->enlace = $item->enlace;
-                        $inmu[$i]->habitaciones = $item->habitaciones;
-                        $inmu[$i]->banos = $item->banos;
-                        $inmu[$i]->m2 = $item->metros2;
-                        $inmu[$i]->tipo = $item->tipo;
-                        $inmu[$i]->telefono = $item->telefono;
-                        $inmu[$i]->latitud = $item->ubicacion[0];
-                        $inmu[$i]->longitud = $item->ubicacion[1];
-                        $inmu[$i]->caracteristicas = $item->caracteristicas;
-                        $inmu[$i]->save();
-                    }
-                }
+        } elseif ($in !== null and strtotime($in->updated_at) < strtotime('-30 days') ) {
+            Inmuebles::where('localizaciones_id', '=', $id_localidad)->where('tipo', '=', $tipo)->delete();
+            $c = '"..\resources\py\scraper_yaencontre.py" '.$localidad." ".$tipo;
+            $result = exec('python '.$c);
+            $json_clean = str_replace("'","\"",$result);
+            $json = json_decode($json_clean);
+            foreach ($json as $item) {
+                $inmueble = new Inmuebles;
+                $inmueble->nombre = $item->nombre;
+                $inmueble->precio = $item->precio;
+                $inmueble->localizaciones_id = $id_localidad;
+                $inmueble->imagenes = $item->imagenes;
+                $inmueble->descripcion = $item->descripcion;
+                $inmueble->enlace = $item->enlace;
+                $inmueble->habitaciones = $item->habitaciones;
+                $inmueble->banos = $item->banos;
+                $inmueble->m2 = $item->metros2;
+                $inmueble->tipo = $item->tipo;
+                $inmueble->telefono = $item->telefono;
+                $inmueble->latitud = $item->ubicacion[0];
+                $inmueble->longitud = $item->ubicacion[1];
+                $inmueble->caracteristicas = $item->caracteristicas;
+                $inmueble->save();
             }
         }
         $in2 = Inmuebles::where('localizaciones_id', '=', $id_localidad)->where('tipo', '=', $tipo)->get();
@@ -123,7 +116,6 @@ class noticiaController extends Controller
 
     public function showLugarInteres($latitud,$longitud)
     {   
-
         $interes = LugaresInteres::where('latitudRadius', '=', $latitud)->first();
         if ($interes === null) {
             $c = '"..\resources\py\google.py" '.$latitud." ".$longitud;
@@ -152,40 +144,32 @@ class noticiaController extends Controller
                     $c->save();
                 }
             }
-        } else {
-            $lugares_interes = LugaresInteres::where('latitudRadius', '=', $latitud)->get();
-            for ($i=0; $i < count($lugares_interes); $i++) { 
-                if (strtotime($lugares_interes[$i]->updated_at) < strtotime('-30 days')) {
-                    $nombre = $lugares_interes[$i]->nombre;
-                    $lugares_interes[$i]->delete();
-                    $c = '"..\resources\py\google.py" '.$latitud." ".$longitud;
-                    $result = exec('python '.$c);
-                    $json_clean = str_replace("'","\"",$result);
-                    $json = json_decode($json_clean);
-                    foreach ($json as $item) {
-                        if ($nombre == $item->nombre) {
-                            $i = new LugaresInteres;
-                            $i->nombre = $item->nombre;
-                            $i->direccion = $item->direccion;
-                            $i->latitud = $item->latitud;
-                            $i->longitud = $item->longitud;
-                            $i->latitudRadius = $latitud;
-                            $i->longitudRadius = $longitud;
-                            $i->telefono = $item->telefono;
-                            $i->puntuacion_media = $item->puntuacion_media;
-                            $i->media_analisis = $item->media_analisis;
-                            $i->save();
-                            $json2 = $item->reviews;
-                            foreach ($json2 as $item2) {
-                                $c = new Comentarios;
-                                $c->autor = $item2->autor;
-                                $c->texto = $item2->texto;
-                                $c->interes_id = $i->id;
-                                $c->puntuacion = $item2->rating;
-                                $c->save();
-                            }
-                        }
-                    }
+        } elseif ($interes !== null and strtotime($interes->updated_at) < strtotime('-30 days') ) {
+            LugaresInteres::where('latitudRadius', '=', $latitud)->delete();
+            $c = '"..\resources\py\google.py" '.$latitud." ".$longitud;
+            $result = exec('python '.$c);
+            $json_clean = str_replace("'","\"",$result);
+            $json = json_decode($json_clean);
+            foreach ($json as $item) {
+                $i = new LugaresInteres;
+                $i->nombre = $item->nombre;
+                $i->direccion = $item->direccion;
+                $i->latitud = $item->latitud;
+                $i->longitud = $item->longitud;
+                $i->latitudRadius = $latitud;
+                $i->longitudRadius = $longitud;
+                $i->telefono = $item->telefono;
+                $i->puntuacion_media = $item->puntuacion_media;
+                $i->media_analisis = $item->media_analisis;
+                $i->save();
+                $json2 = $item->reviews;
+                foreach ($json2 as $item2) {
+                    $c = new Comentarios;
+                    $c->autor = $item2->autor;
+                    $c->texto = $item2->texto;
+                    $c->interes_id = $i->id;
+                    $c->puntuacion = $item2->rating;
+                    $c->save();
                 }
             }
         }
